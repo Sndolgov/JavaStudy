ec3a70c6b123ed320c43a1c6e7b12571e9784127
Sndolgov <Dolgov.Sergey84@gmail.com>
2017-10-16 17:33:35 +0300

    Delete crud

--- CRUD/mycrud/creat_db/add_users.sql
+++
@@ -1,8 +0,0 @@
-
-INSERT INTO `usersbase`.`users` (`name`, `age`, `isAdmin`) VALUES  ( 'Pit', '25',FALSE);
-INSERT INTO usersbase.users (name,age, isadmin) VALUES ( 'Mike', '27', TRUE);
-INSERT INTO usersbase.users (name,age, isadmin) VALUES ( 'Jon', '21', FALSE);
-INSERT INTO usersbase.users (name,age, isadmin) VALUES ( 'Sam', '25', TRUE);
-INSERT INTO usersbase.users (name,age, isadmin) VALUES ( 'David', '19', FALSE);
-
-
